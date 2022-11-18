2cdaa8cd8c5986eb079ec0bf11514da967efc1f6
Orlan29 <bricebouadou@gmail.com>
2022-11-18 15:52:02 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PresentationComponent } from './presentation/presentation.component';
+import { DishListComponent } from './dish-list/dish-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    PresentationComponent,
+    DishListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
