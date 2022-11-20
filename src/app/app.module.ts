25dc533dbd559879ba3c6bcac1e66a46932e0e80
Orlan_29 <bricebouadou@gmail.com>
2022-11-20 23:34:16 +0000

    Presentation and menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { PresentationComponent } from './presentation/presentation.component';
 import { DishListComponent } from './dish-list/dish-list.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
     PresentationComponent,
-    DishListComponent
+    DishListComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
